b85bcb2525bcdd0071c8ef508282bd90641e18ce
Jagadeeshkarlapudi <118984594+Jagadeeshkarlapudi@users.noreply.github.com>
2026-02-17 12:04:32 +0530

    Update views.py added user

--- loginpp/views.py
+++ loginpp/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, logout
+from django.contrib.auth.models import User,auth
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
